88883dfa7e6e9ce452b35e2eacce05fb72875dee
Jeremy <jetsai@hmc.edu>
2020-03-18 16:26:06 -0700

    Successfully update popup after closing/opening

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//-------------------------------------Button Logic--------------------------------------------------//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Restore previous state of popup (sessions and user preferences) stored in chrome.storage when new tab is opened
 function restore_user() {
   // Restore popup HTML with previous state 
   chrome.storage.sync.get("prev_state", function(state) {
-    // State is null only upon download, only change if not null
-    if (state != null) {
-
+    // State is null only upon download, only update if not null
+    if (state.prev_state != null) {
+      refresh_popup(state.prev_state);
     }
   });
 
@@ -14,33 +18,25 @@ function restore_user() {
   });
 }
 
-// Add a session row into the popup menu
-function add_row(name) {
-  // Inject row into popup
-  let menu = document.getElementById("popup-background");
+// Clear current popup HTML and replace with new state
+function refresh_popup(state) {
+  // Make dummy div to store inner HTML
   let div = document.createElement("div");
-  div.id = name;
-  div.innerHTML = name;
-  menu.appendChild(div);
+  div.innerHTML = state;
+  let new_state = div.firstChild;
 
-  // Update previous state in chrome.storage
-  chrome.storage.sync.get("prev_state", function(state) {
+  // // Remove current popup
+  // var old_sessions = document.getElementById("sessions-body");
+  // old_sessions.removeChild(old_sessions);
 
-  });
-
-  // Update names array in chrome.storage
-  chrome.storage.sync.get("names_arr", function(arr) {
-
-  });
+  // Append state into correct position
+  /// state.id = "sessions-body";
+  let anchor = document.getElementById("sessions"); // Reference point for insertion
+  anchor.appendChild(new_state);
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-//-------------------------------------Button Logic--------------------------------------------------//
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Saving current tab and updating popup
 function save_tab() {
-  console.log("tab pressed");
   // Get url of user's current tab
   chrome.tabs.query({
     active: true, currentWindow: true
@@ -48,10 +44,14 @@ function save_tab() {
       console.log(tabs);
       let tab = tabs[0];
       let url = [tab.url];
+      // Prompt user for new session name
       let name = "tab";
 
       // Update session in chrome.storage
-      chrome.storage.sync.set({name: url});
+      chrome.storage.sync.set({[name]: url});
+      chrome.storage.sync.get([name], function(item){
+        console.log(item[name]);
+      });
 
       // Update popup menu
       add_row(name);
@@ -60,13 +60,58 @@ function save_tab() {
 
 // Saving current session (all tabs of window)
 function save_session() {
+  // Get all urls of user's current window
+
+  // Prompt user for new session name
   alert("session pressed");
 }
 
 // Opening settings HTML page
 function open_settings() {
+  // Open settings html page
   alert("settings pressed");
 }
+
+// Add a session row into the popup menu
+function add_row(name) {
+  // Inject row into popup
+  let menu = document.getElementById("sessions-body");
+  let div = document.createElement("div");
+  div.id = name;
+  div.innerHTML = name;
+  menu.appendChild(div);
+
+  // Create event listener for clicking on row session
+
+
+  // Update previous state in chrome.storage
+  let new_state = document.getElementById("sessions-body");
+
+  chrome.storage.sync.set({"prev_state": new_state.outerHTML});
+
+
+  // Update names array in chrome.storage
+  chrome.storage.sync.get("names_arr", function(arr) {
+    let names_arr = arr.names_arr;
+    names_arr.push(name);
+    chrome.storage.sync.set({names_arr: names_arr});
+  });
+}
+
+// Show form for session name
+function prompt_name() {
+  
+  
+  // Check if name already exists in name array
+
+
+}
+
+// Right click on row to rename
+
+
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //-------------------------------------Event Listeners-----------------------------------------------//
