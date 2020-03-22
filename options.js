96baef47ad6d67c9e9bae86b948a70863bc97596
sergioleonardo <sergiogarciajr@icloud.com>
2020-03-22 12:54:43 -0700

    Added options checkbox

--- options.js
+++ options.js
@@ -1,1 +1,9 @@
+var checkbox = document.querySelector("input[type=checkbox]");
 
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        console.log("checked");
+    } else {
+        console.log("not checked");
+    }
+});
