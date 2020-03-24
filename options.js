5f91628fbd6eee19f966970c35f258f509ba9595
sergioleonardo <sergiogarciajr@icloud.com>
2020-03-23 17:51:01 -0700

    Added options function

--- options.js
+++ options.js
@@ -1,9 +1,24 @@
-var checkbox = document.querySelector("input[type=checkbox]");
+var checkbox1 = document.querySelector(".option-1-input");
 
-checkbox.addEventListener( 'change', function() {
+changeOption = (key, val) => {
+    chrome.storage.sync.get("preference_arr", function(arr) {
+        let preference_arr = arr.preference_arr;
+
+        preference_arr[key] = val;
+        chrome.storage.sync.set({"preference_arr": preference_arr});
+        console.log(preference_arr);
+      });
+};
+
+checkbox1.addEventListener( 'change', function() {
     if(this.checked) {
-        console.log("checked");
+        changeOption(0, true);
     } else {
-        console.log("not checked");
+        changeOption(0, false);
     }
 });
+
+
+
+
+
