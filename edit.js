ecd66b5388ea5cc20ef677bd37780c3e85a50eb5
sergioleonardo <sergiogarciajr@icloud.com>
2020-03-26 19:03:04 -0700

    Improve edit page

--- edit.js
+++ edit.js
@@ -1,6 +1,7 @@
-alert(old_name);
 
 // Navigates back to popup.html on click of arrow button
-document.getElementById("edit-back").addEventListener("click",() => {
+let close_edit = document.getElementById("edit-back")
+
+close_edit.addEventListener("click",() => {
   window.location.href = "popup.html";
-})
+});
