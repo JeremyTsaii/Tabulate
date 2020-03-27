05160fb47679126af99692da5a6fcdd4aa6b51ec
Jeremy <jetsai@hmc.edu>
2020-03-26 18:51:20 -0700

    Create files for edit session

---
+++ edit.js
@@ -0,0 +1,6 @@
+alert(old_name);
+
+// Navigates back to popup.html on click of arrow button
+document.getElementById("edit-back").addEventListener("click",() => {
+  window.location.href = "popup.html";
+})
